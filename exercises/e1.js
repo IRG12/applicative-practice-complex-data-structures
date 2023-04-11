90f74880246f265a44e7a4f39ec77703347107a9
Isaiah G <isaiahrg12@gmail.com>
2023-04-11 07:17:09 -0500

    Practicing Complex Data Structures
    Passed tests for 'e1.js'

--- exercises/e1.js
+++ exercises/e1.js
@@ -6,9 +6,12 @@ import { data } from "../data/data";
 
 export function getPlanetNames(data) {
   // Your code goes here...
+  const planets = data.planets;
+  const planetNames = planets.map((planet) => planet.name);
+
+  console.log(planetNames);
+  return planetNames;
 }
-
-
 
 // === TEST YOURSELF ===
 // Once you're finished run the test with "npm run test-1"
