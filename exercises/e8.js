052664eb70ea88d825fb55a9864d675890970493
Isaiah G <isaiahrg12@gmail.com>
2023-04-11 13:14:01 -0500

    Practicing Complex Data Structures
    Passed tests for 'e8.js'

--- exercises/e8.js
+++ exercises/e8.js
@@ -6,9 +6,12 @@ import { data } from "../data/data";
 
 export function findPlanetNameByMoon(data, moonName) {
   // Your code goes here...
+  const planets = data.planets;
+  const planetNames = planets.find(
+    (planet) => planet.moons && planet.moons.includes(moonName)
+  );
+  return planetNames.name;
 }
-
-
 
 // === TEST YOURSELF ===
 // Once you're finished run the test with "npm run test-8"
