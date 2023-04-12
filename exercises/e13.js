56d3101c0982ceb66fc914b215cd7dda20170908
Isaiah G <isaiahrg12@gmail.com>
2023-04-12 09:27:19 -0500

    Practicing Complex Data Structures
    Passed tests for 'e13.js'

--- exercises/e13.js
+++ exercises/e13.js
@@ -6,9 +6,13 @@ import { data } from "../data/data";
 
 export function getAveragePlanetsTemperature(data) {
   // Your code goes here...
+  const planets = data.planets;
+  let totalTempOfAllPlanets = 0;
+  const tempOfAllPlanets = planets.map((planet) => {
+    totalTempOfAllPlanets += planet.avgTemp;
+  });
+  return totalTempOfAllPlanets / planets.length;
 }
-
-
 
 // === TEST YOURSELF ===
 // Once you're finished run the test with "npm run test-13"
