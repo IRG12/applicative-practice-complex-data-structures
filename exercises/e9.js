2c85475e8f92deb1bf55554dee8aed840433999b
Isaiah G <isaiahrg12@gmail.com>
2023-04-17 08:17:15 -0500

    Practicing Complex Data Structures
    Refactored and passed tests for 'e9.js'

--- exercises/e9.js
+++ exercises/e9.js
@@ -5,12 +5,7 @@ import { data } from "../data/data";
 // Return example: { key1: value1, key2: value2, ... , keyN: valueN }
 
 export function getEarthData(data) {
-  const planets = data.planets;
-  const planetObj = planets.find(
-    (planet) => planet.id && planet.id === "earth"
-  );
-
-  return planetObj;
+  return data.planets.find((planet) => planet.id && planet.id === "earth");
 }
 
 // === TEST YOURSELF ===
